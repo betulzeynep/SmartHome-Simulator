d923cbbfc8a4ac08a6fbfbfb25ac843ebbc8b894
LogicAndLines <betulzeynepseyis@gmail.com>
2026-03-16 16:56:13 +0100

    Initial Commit

---
+++ SmartHome Simulator/SmartHome_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHome_SimulatorApp.swift
+//  SmartHome Simulator
+//
+//  Created by Zeynep Turnalı on 16.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SmartHome_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartHome SimulatorUITests/SmartHome_SimulatorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SmartHome_SimulatorUITestsLaunchTests.swift
+//  SmartHome SimulatorUITests
+//
+//  Created by Zeynep Turnalı on 16.03.2026.
+//
+
+import XCTest
+
+final class SmartHome_SimulatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
